3254f222a6be2add5130bb40f362dae5622a83cb
Anna <karlovskaya@gmail.com>
2018-10-18 11:18:29 -0700

    add CSS modules and SASS

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 1. App accepts mm/dd date of birth format and displays corresponding zodiac sign
 2. Input validation
 3. "react-google-charts" module to display data as a pie chart
+4. Using both Sass and CSS Module
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.module.scss';
 import Inputs from './components/Inputs/Inputs';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.App}>
+          <h1>Title here</h1>
+
           <Inputs />
       </div>
     );
