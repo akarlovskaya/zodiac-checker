6128ae30563c0e3e6dc9415baf9ee50c177c04e1
Anna <karlovskaya@gmail.com>
2018-10-23 15:33:05 -0700

    Updates

---
+++ precache-manifest.c786da292a4f9607efbb3422f880444f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a5ca4aa782fc20345b",
+    "url": "/zodiac-checker/static/js/runtime~main.22a5ca4a.js"
+  },
+  {
+    "revision": "6418267737e511ca9054",
+    "url": "/zodiac-checker/static/js/main.64182677.chunk.js"
+  },
+  {
+    "revision": "8876341cba48fe12a40a",
+    "url": "/zodiac-checker/static/js/1.8876341c.chunk.js"
+  },
+  {
+    "revision": "6418267737e511ca9054",
+    "url": "/zodiac-checker/static/css/main.8d3e7d28.chunk.css"
+  },
+  {
+    "revision": "4a7dd2a15720adf2a0dcb42091a54e4c",
+    "url": "/zodiac-checker/index.html"
+  }
+];
